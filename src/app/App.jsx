07874d78aa6662741dda6621c980083e71ac13fd
LeoVo <Leo.Volkov156@gmail.com>
2025-02-13 23:10:50 +0300

    Revert "Режим мок"
    This reverts commit b2880dc594be135ccd71fbcfd73f7c18dcf2800c.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import AppRoutes from './routes/Routes.jsx'
-import { store, persistor } from '@/app/store/configureStore.ts'
+import { store, persistor } from '@store/configureStore.ts'
 import './styles/index.css'
 import './styles/normalaiz.css'
 
@@ -16,7 +16,7 @@ async function enableMocking() {
    * Если IS_MOCKING = true, то мок-запросы включены.
    * Если IS_MOCKING = false, то мок-запросы выключены.
    */
-  const IS_MOCKING = true
+  const IS_MOCKING = false
 
   if (IS_MOCKING) {
     const { worker } = await import('./test/mocks/browser.js')
