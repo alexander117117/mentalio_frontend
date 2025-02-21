7754685b119745ba57aeaa0264da28b0be39b421
Лев <115810918+Leo-Volkov@users.noreply.github.com>
2025-02-20 20:41:07 -0800

    add features handleDeleteTopic

--- src/features/InfoUserFolder/index.tsx
+++ src/features/InfoUserFolder/index.tsx
@@ -3,6 +3,8 @@ import { TopicInfo } from './components/TopicInfo'
 import { flexColumnStartClass } from '@/shared/lib/classNames'
 import { folderClass } from './lib'
 import { TopicsItem } from '@/entities/folder/lib/types'
+import { ReactComponent as CloseIcon } from '@/shared/assets/images/assets/close_icon.svg?react'
+
 
 interface InfoUserFolderProps {
   dataTopic: TopicsItem
@@ -12,6 +14,9 @@ export function InfoUserFolder({ dataTopic, idFolder }: InfoUserFolderProps) {
   const handleOpenFolderPage = (idFolder: string | number, idTopic: string | number) => {
     console.log('OpenFolderPage: ', { idFolder, idTopic })
   }
+  const handleDeleteTopic = (idTopic: string | number) => {
+    console.log('DeleteTopic id: ', { idTopic })
+  }
   return (
     <>
       <div className={folderClass}>
@@ -19,6 +24,7 @@ export function InfoUserFolder({ dataTopic, idFolder }: InfoUserFolderProps) {
           <TopicInfo dataTopic={dataTopic} />
         </div>
 
+        <CloseIcon title="delete topic" onClick={() => handleDeleteTopic(dataTopic.id)} />
         <ButtonControlFolder onClick={() => handleOpenFolderPage(idFolder, dataTopic.id)}>
           Посмотреть
         </ButtonControlFolder>
