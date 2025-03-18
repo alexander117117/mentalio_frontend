cf42d124a9dcd5889005b5fd6211301a43752f15
Лев <115810918+Leo-Volkov@users.noreply.github.com>
2025-03-18 03:06:14 +0300

    Удаление старого кода

--- src/features/CreateFolder/lib/handeles/index.ts
+++
@@ -1,1 +0,0 @@
-export { handleOnSubmit } from './handleOnSubmit'
--- src/shared/ui/buttons/ButtonAddWordTranslate/index.tsx
+++
@@ -1,7 +0,0 @@
-import style from './index.module.css'
-interface ButtonAddWordTranslateProps {
-  children: React.ReactNode
-}
-export function ButtonAddWordTranslate({ children }: ButtonAddWordTranslateProps) {
-  return <button className={style.button_add_word_translate}>{children}</button>
-}
