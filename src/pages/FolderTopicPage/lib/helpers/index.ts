2862cf35c97754e3ebe700053baaecd237db93a4
Лев <115810918+Leo-Volkov@users.noreply.github.com>
2025-03-18 03:14:35 +0300

    FolderTopicPage

---
+++ src/pages/FolderTopicPage/lib/helpers/index.ts
@@ -0,0 +1,35 @@
+import { AppDispatch } from '@/app/store/configureStore'
+import { TopicsItem } from '@/entities/folder/lib/types'
+import { getTopic, setTopicData } from '@/entities/topic/model/store'
+import { Id } from '@/shared/types'
+import { Location } from 'react-router'
+
+interface GetTopicDataParams {
+  dispatch: AppDispatch
+  location: Location
+  idFolder: Id | undefined
+  idTopic: Id | undefined
+}
+export async function getTopicData({ dispatch, location, idFolder, idTopic }: GetTopicDataParams) {
+  if (location.state?.dataTopic) {
+    const topicFromLocation = location.state.dataTopic as TopicsItem
+    dispatch(
+      setTopicData({
+        dataTopic: topicFromLocation,
+        idFolder,
+      }),
+    )
+  } else if (idFolder && idTopic) {
+    try {
+      const response = await dispatch(getTopic({ idFolder, idTopic })).unwrap()
+      dispatch(
+        setTopicData({
+          dataTopic: response.dataTopic,
+          idFolder,
+        }),
+      )
+    } catch (err) {
+      console.error('Error while fetching topic', err)
+    }
+  }
+}
