2848775623281ab92fc1f5893b710d03159f8d69
Лев <115810918+Leo-Volkov@users.noreply.github.com>
2025-04-02 00:52:59 +0300

    InputChecked

--- src/shared/ui/inputs/InputChecked/index.tsx
+++ src/shared/ui/inputs/InputChecked/index.tsx
@@ -1,8 +1,10 @@
 interface InputCheckedProps {
   title: string
   nameInput: string
+  checked?: boolean
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
-export function InputChecked({ title, nameInput }: InputCheckedProps) {
+export function InputChecked({ title, nameInput, checked, onChange }: InputCheckedProps) {
   return (
     <div className="flex items-center justify-between bg-popup px-[15px] py-5 rounded-[10px]">
       <label htmlFor={title} className="flex flex-col">
@@ -11,6 +13,8 @@ export function InputChecked({ title, nameInput }: InputCheckedProps) {
       <input
         id={title}
         type="checkbox"
+        checked={checked}
+        onChange={onChange}
         className="w-5 sm:w-[40px] aspect-square bg-transparent outline-none border border-borderDark rounded-[10px]"
         name={nameInput}
       />
