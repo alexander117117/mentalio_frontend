76f048adecf951b68dafae1c62dfb1e8525a605d
Лев <115810918+Leo-Volkov@users.noreply.github.com>
2025-04-05 16:01:44 +0300

    inputs

--- src/shared/ui/inputs/InputChecked/index.tsx
+++ src/shared/ui/inputs/InputChecked/index.tsx
@@ -1,10 +1,10 @@
+import { UseFormRegisterReturn } from 'react-hook-form'
+
 interface InputCheckedProps {
   title: string
-  nameInput: string
-  checked?: boolean
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  register: UseFormRegisterReturn
 }
-export function InputChecked({ title, nameInput, checked, onChange }: InputCheckedProps) {
+export function InputChecked({ title, register }: InputCheckedProps) {
   return (
     <div className="flex items-center justify-between bg-popup px-[15px] py-5 rounded-[10px]">
       <label htmlFor={title} className="flex flex-col">
@@ -13,10 +13,8 @@ export function InputChecked({ title, nameInput, checked, onChange }: InputCheck
       <input
         id={title}
         type="checkbox"
-        checked={checked}
-        onChange={onChange}
         className="w-5 sm:w-[40px] aspect-square bg-transparent outline-none border border-borderDark rounded-[10px]"
-        name={nameInput}
+        {...register}
       />
     </div>
   )
